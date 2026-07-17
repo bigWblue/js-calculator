eaa702244f9a3726d26fedc1acee38bc57037c6a
bigWblue <258706296+bigWblue@users.noreply.github.com>
2026-07-17 00:53:02 +0000

    Added function to extra buttons
    The extra buttons: parentheses, power of 2 and square root all work now and an expression involving them should work as long as the power and square roots are of whole numbers instead of expressions, e.g. 5² instead of (2+3)².

--- calculator.js
+++ calculator.js
@@ -32,6 +32,98 @@ function fixFloat(firstOpr) {
     return newOperation.join(firstOpr[operator]);
 }
 
+function handleSquareRoot(operation) {
+    const sqrtRegex = /√\(((?:\d+\.)?\d+[^)]*)\)/;
+    const match = operation.match(sqrtRegex);
+    if (!match) {
+        return operation;
+    }
+    if (match[1] < 0 || isNaN(match[1])) {
+        return 'Error'
+    }
+    const squareRoot = Math.sqrt(Number(match[1]));
+    const replaced = operation.replace(`√(${match[1]})`, `${squareRoot}`)
+    return replaced
+}
+
+function handleSquare(operation) {
+    const squareRegex = /(?:\d+\.)?\d+(?=²)/;
+    const match = operation.match(squareRegex);
+    if (!match || /(?<=\))²/.test(operation)) {
+        return 'Error';
+    }
+    const squared = match[0] * match[0];
+    return operation.replaceAll(`${match[0]}²`, squared)
+}
+
+
+function validateParentheses(operation) {
+    let isValid = true;
+    const countParentheses = {
+        'open': 0,
+        'close': 0,
+        'inOrder': '',
+        'total': 0
+    }
+    for (const char of operation) {
+        if (char === '(') {
+            countParentheses['open'] += 1;
+            countParentheses['total'] += 1;
+            countParentheses['inOrder'] += char
+        }
+        else if (char === ')') {
+            countParentheses['close'] += 1;
+            countParentheses['total'] += 1;
+            countParentheses['inOrder'] += char
+        }
+    }
+
+    if (countParentheses.open !== countParentheses.close || countParentheses.total % 2 !== 0) {
+        isValid = false;
+        return isValid;
+    }
+
+    let pairs = countParentheses['inOrder'].slice();
+    for (let i = 0; i < (countParentheses.total / 2); i++) {
+        const pairRemoved = pairs.replace('()', '');
+        pairs = pairRemoved;
+    }
+
+    if (pairs !== '') {
+        isValid = false;
+        return isValid;
+    }
+    return isValid
+}
+
+function handleParentheses(operation) {
+    if (!validateParentheses(operation)) {
+        return 'Error'
+    }
+    const parenthesesRegex = /\([-+/*\d]+\)/;
+    const match = operation.match(parenthesesRegex);
+    if (!match) {
+        return 'Error'
+    }
+    const innerOperation = match[0].slice(1, match[0].length-1);
+    let innerResult = innerOperation;
+
+    let maxCap = 0;
+    while (/[-+*/]/.test(innerResult)) {
+        maxCap += 1;
+        innerResult = multiplyOrDivide(innerResult);
+        if (!isNaN(innerResult) || maxCap >= 10000) {
+            break
+        }
+        innerResult = addOrSubtract(innerResult);
+    }
+    if (maxCap >= 100 || innerResult.includes('Error')) {
+        return 'Error'
+    }
+    const replaced = operation.replaceAll(match[0], innerResult);
+    return replaced;
+}
+
 function addOrSubtract(operationStr) {
     const firstOperationRegex = /-?\d*\.?\d+[+-]\d*\.?\d+/;
     const match = operationStr.match(firstOperationRegex);
@@ -48,6 +140,9 @@ function addOrSubtract(operationStr) {
     let [operandOne, operandTwo] = firstOpMatch.split(/(?<=\d)[+-](?=-?\d)/);
     operandOne = Number(operandOne);
     operandTwo = Number(operandTwo);
+    if (Number.isNaN(operandOne) || Number.isNaN(operandTwo)) {
+        return 'Error'
+    }
     let total = 0;
     
     if (firstOpMatch.includes('+')) {
@@ -95,18 +190,51 @@ function multiplyOrDivide(operationStr) {
 
 export function evaluateDisplay(operation) {
     let total = operation;
+    let loopCount = 0;
+
+    while (total.includes('√')) {
+        loopCount += 1;
+        const prev = handleSquareRoot(total);
+        total = prev;
+        if (loopCount > 20 || total === 'Error') {
+            break;
+        }
+    }
+    
+    while (total.includes('²')) {
+        loopCount += 1;
+        const prev = handleSquare(total);
+        total = prev;
+        if (loopCount > 20 || total === 'Error') {
+            break;
+        }
+    }
+
+    while (total.includes('(') || total.includes(')')) {
+        loopCount += 1;
+        const prev = handleParentheses(total);
+        if (prev === 'Error' || loopCount > 20) {
+            break;
+        }
+        total = prev;
+    }
+
     while (total.includes('*') || total.includes('/')) {
+        loopCount += 1;
+        if (loopCount > 20) {break}
         const parsedPrecedence = multiplyOrDivide(total);
         total = parsedPrecedence;
         if (['*', '/', '.'].includes(total[total.length-1]) || /[*/+]{2,}|-[*/+]+/.test(operation)) {
             total = 'Error';
+            break;
         }
     }
     
     while (isNaN(total)) {
+        loopCount += 1;
         const partialResult = addOrSubtract(total);
         total = partialResult;
-        if (total === 'Error') {
+        if (total === 'Error' || loopCount > 20) {
             break;
         }
     }
